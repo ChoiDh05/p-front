8c9ec27fe52489b280d7256ff97aeb58a0e8c7f3
hamandy2000 <minji9306@gmail.com>
2024-10-23 11:13:05 +0900

    interview,matching update

--- static/js/matching/test.js
+++ static/js/matching/test.js
@@ -1,6 +1,15 @@
 const optionSelects = document.querySelectorAll(".tab-section li");
 const optionContents = document.querySelectorAll(".option-content ");
+const map = document.querySelector("#map");
 
+map.addEventListener("click", (e) => {
+    new daum.Postcode({
+        oncomplete: function (data) {
+            // 팝업에서 검색결과 항목을 클릭했을때 실행할 코드를 작성하는 부분입니다.
+            // 예제를 참고하여 다양한 활용법을 확인해 보세요.
+        },
+    }).open();
+});
 optionSelects.forEach((optionSelect, index) => {
     optionSelect.addEventListener("click", (e) => {
         // console.log(e.target);
